32461d3efcb4c96177aa9b8bf89a1b82902c7df0
Lynnette Ng <lynnette-ng@outlook.com>
2015-09-08 09:28:17 +0000

    Fixed country changed error

--- js/main.js
+++ js/main.js
@@ -18,7 +18,20 @@ var weatherColElement;
 var weather;
 var weatherColour;
 
+var sel;
+var val;
+var offset;
+var countryLoc;
+var now;
+
 window.onload = function(e) {
+    populateCountries("country2");
+    sel = document.getElementById("country2");
+    sel.value = "Singapore";
+    countryLoc = sel.options[sel.selectedIndex].text;
+    
+    now = new Date();
+
     timeBox = document.getElementById('timeBox');
     timeElement = document.getElementById('time');
     timeColElement = document.getElementById('timeColour');
@@ -31,11 +44,23 @@ window.onload = function(e) {
     weatherElement = document.getElementById('weather');
     weatherColElement = document.getElementById('weatherColour');
     
+    //countryChanged();
+    
 	updateClock();
 }
 
 function updateClock() {
     now = new Date();
+    var utc = now.getTime() - (now.getTimezoneOffset() * 60000);
+    val = sel.selectedIndex; 
+    
+    var localoffset = -(now.getTimezoneOffset()/60);
+    var destoffset = timezone_arr[val-1];
+    
+    offset = destoffset-localoffset-1;
+    console.log("Local Offset: " + localoffset + " Dest Offset: " + destoffset + " offset: " + offset);
+    
+    now = new Date(utc + (3600000*offset));
     
     time = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2) + ':' + ('0' + now.getSeconds()).slice(-2);
     timeColour = '#' + time.replace(/:/g, '');
@@ -51,10 +76,11 @@ function updateClock() {
     dateColElement.innerHTML = dateColour;
     dateBox.style.backgroundColor = dateColour;
     
+    
     $.simpleWeather({
         zipcode: '',
-        woeid: 1062617,   //Currently Singapore
-        location: '',
+        woeid: '',   //Singapore woeid: 1062617
+        location: countryLoc,
         unit: 'c',
         success: function (weather) {
             var temp = weather.temp;
@@ -75,6 +101,7 @@ function updateClock() {
             },
             error: function (error) {
                 console.log("Error in getting weather");
+                weatherElement.innerHTML = "Whoops, error getting weather information!";
             }
     });
 
@@ -90,3 +117,46 @@ function setBackgroundColour() {
     
     document.body.style.backgroundColor = result_color;
 }
+
+function countryChanged() {
+    val = sel.selectedIndex; 
+    countryLoc = sel.options[sel.selectedIndex].text;
+    
+}
+
+var timezone_arr = new Array(4.30, 1.00, -11,
+1.0, -4.00, 0, -4, -3, 4,
+-4, 5, 5, 1, 4, -5,
+-3, 6, -4, 3, 1, -6, 1, -4,
+6, -4, 1, 2, -5, -4,
+8, 2, 0, 6.30, 2, 7, 1, -8,
+-1, -5, 1, 1, -5, 8, 7,
+-8, 6.30, -5, 3, 1,
+1, -10, -6, 0, 1, -5, 2,
+1, 1, 3, -4, -4, -6, 2,
+-6, 1, 3, 2, 3, 3,
+-3, 0, 12, 2, -10, -3, 
+-10, 3, 1, 0, 3,
+3, 1, 0, 1, 4, 2, -4, -4,
+-4, 10, -6, 0, 0, 0, -4, -5,
+5, 1, -6, 8, -12,
+1, 0, 5.30, 7, 3.30, 3, 0, 0, 2, 1,
+-5, 1, 9, -11, 0, -10, 2, 3, 
++5, 3, 12, 8.30, 9, 3, 6, 7, 2,
+2, 2, 0, 2, 1, 2, 1, 8, 
+1, 3, 2, 8, 5, 0, 1,
+0, 4, -4, 0, 4, 3, -8,
+10, -11, 2, 1, 7, -4, 
+0, 2, 1, 12, 5.45, -4, -4, 11,
+12, -6, 1, 1, -11, 11.30, 10, 1,
+4, 5, 9, -5, 10, -4, -5, 8, -8, 
+1, -1, -4, 3, 4, 2, 2, 2, 0,
+-4, -4, -4, 0, 13,
+1, 0, 1, 1, 0, 4, 0,
+8, 1, 1, 11, 3, 3,
+-1, 0, 0, 5.30, 3, -3,
+1, 2, 1, 1, -2, 8, 5, 3, 7,
+-4, 0, 13, 13, -4, 1, 2, 5, 12, 3, 
+3, 4, 0, 0, 10, 5, 11, -4.30,
+3, -4, 4, 4, 0, 0, 3, 0, 
+2, 2, 2);
