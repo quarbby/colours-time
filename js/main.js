5c7b9c1bae7bbf1a3019fc1a8bbe69979db2233f
Lynnette Ng <lynnette-ng@outlook.com>
2016-04-23 03:37:16 +0000

    Fixed weather and JSON countries

--- js/main.js
+++ js/main.js
@@ -18,16 +18,28 @@ var weatherColElement;
 var weather;
 var weatherColour;
 
+var locationOfUser = '';
+var geolocation = false;
+
 var sel;
 var val;
 var offset;
 var countryLoc;
 var now;
 
+var countriesData = [];
+
 window.onload = function(e) {
-    populateCountries("country2");
-    sel = document.getElementById("country2");
+    //populateCountries("country2");
+    
+    readData();
+    populateCountriesFromJSON("countryList");
+    //console.log(countriesData);
+    
+    // Set default to Singapore 
+    sel = document.getElementById("countryList");
     sel.value = "Singapore";
+    countryChanged();
     countryLoc = sel.options[sel.selectedIndex].text;
     
     now = new Date();
@@ -44,9 +56,39 @@ window.onload = function(e) {
     weatherElement = document.getElementById('weather');
     weatherColElement = document.getElementById('weatherColour');
     
-    //countryChanged();
+    // Get user's location
+    if ("geolocation" in navigator) {
+      $('.js-geolocation').show(); 
+    } else {
+      $('.js-geolocation').hide();
+    }
+
+    locationOfUser = countriesData[val].latlong;
     
+    navigator.geolocation.getCurrentPosition(function(position) {
+        //console.log(position);
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        locationOfUser = latitude + ',' + longitude;
+        geolocation = true;
+    },
+    function(error) {
+        geolocation = false;
+        console.log("User said no geolocation");
+    });
+
 	updateClock();
+}
+
+function readData() {
+    $.ajax({
+      url: 'data/countries.json',
+      async: false,
+      dataType: 'json',
+      success: function (data) {
+        countriesData = data.countries;
+      }
+    });
 }
 
 function updateClock() {
@@ -55,9 +97,12 @@ function updateClock() {
     val = sel.selectedIndex; 
     
     var localoffset = -(now.getTimezoneOffset()/60);
-    var destoffset = timezone_arr[val-1];
+    //var destoffset = timezone_arr[val-1];
+    //console.log(countriesData[val]);
+    var destoffset = countriesData[val-1].timezone_offset;
     
-    offset = destoffset-localoffset-1;
+    //offset = destoffset-localoffset-1;
+    offset = destoffset - localoffset;
     //console.log("Local Offset: " + localoffset + " Dest Offset: " + destoffset + " offset: " + offset);
     
     now = new Date(utc + (3600000*offset));
@@ -76,33 +121,42 @@ function updateClock() {
     dateColElement.innerHTML = dateColour;
     dateBox.style.backgroundColor = dateColour;
     
-    
-    $.simpleWeather({
-        zipcode: '',
-        woeid: '1062617',   //Singapore woeid: 1062617
-        unit: 'c',
-        success: function (weather) {
-            var temp = weather.temp;
-            var humidity = weather.humidity;
-            var windSpeed = weather.wind.speed;
-            
-            var weatherString = "Temp: " + temp + "&deg;"+ weather.units.temp +
-                                "<br>Humidity: " + humidity +
-                                "<br>Wind Speed: " + windSpeed;
-            
-            weather = weather.currently;
-            weatherString += "<br>" + weather;
-            weatherElement.innerHTML = weatherString;
-            
-            weatherColour = '#' + temp + humidity + ('' + windSpeed.replace('.', '')).slice(0,2);
-            weatherColElement.innerHTML = weatherColour;
+    //locationOfUser = '' + latitude + ',' + longitude;
 
-            },
-            error: function (error) {
-                console.log("Error in getting weather");
-                weatherElement.innerHTML = "Whoops, error getting weather information!";
-            }
-    });
+    if (geolocation == false && locationOfUser == '') {
+        weatherElement.innerHTML = "Whoops, geolocation not allowed! ";
+    }
+    
+    else {
+        $.simpleWeather({
+            location: locationOfUser,
+            woeid: '',   //Singapore woeid: 1062617
+            unit: 'c',
+            success: function (weather) {
+                var temp = weather.temp;
+                var humidity = weather.humidity;
+                var windSpeed = weather.wind.speed;
+                
+                var weatherString = "Temp: " + temp + "&deg;"+ weather.units.temp +
+                                    "<br>Humidity: " + humidity +
+                                    "<br>Wind Speed: " + windSpeed;
+                                    
+                //var city = weather.city + ", " + weather.region;
+                
+                weather = weather.currently;
+                weatherString += "<br>" + weather;
+                weatherElement.innerHTML = weatherString;
+                
+                weatherColour = '#' + temp + humidity + ('' + windSpeed.replace('.', '')).slice(0,2);
+                weatherColElement.innerHTML = weatherColour;
+    
+                },
+                error: function (error) {
+                    console.log("Error in getting weather");
+                    weatherElement.innerHTML = "Whoops, error getting weather information!";
+                }
+        });        
+    }
 
     setBackgroundColour();
 
@@ -121,42 +175,15 @@ function setBackgroundColour() {
 function countryChanged() {
     val = sel.selectedIndex; 
     countryLoc = sel.options[sel.selectedIndex].text;
-    
+    locationOfUser = countriesData[val].latlong;
 }
 
-var timezone_arr = new Array(4.30, 1.00, -11,
-1.0, -4.00, 0, -4, -3, 4,
--4, 5, 5, 1, 4, -5,
--3, 6, -4, 3, 1, -6, 1, -4,
-6, -4, 1, 2, -5, -4,
-8, 2, 0, 6.30, 2, 7, 1, -8,
--1, -5, 1, 1, -5, 8, 7,
--8, 6.30, -5, 3, 1,
-1, -10, -6, 0, 1, -5, 2,
-1, 1, 3, -4, -4, -6, 2,
--6, 1, 3, 2, 3, 3,
--3, 0, 12, 2, -10, -3, 
--10, 3, 1, 0, 3,
-3, 1, 0, 1, 4, 2, -4, -4,
--4, 10, -6, 0, 0, 0, -4, -5,
-5, 1, -6, 8, -12,
-1, 0, 5.30, 7, 3.30, 3, 0, 0, 2, 1,
--5, 1, 9, -11, 0, -10, 2, 3, 
-+5, 3, 12, 8.30, 9, 3, 6, 7, 2,
-2, 2, 0, 2, 1, 2, 1, 8, 
-1, 3, 2, 8, 5, 0, 1,
-0, 4, -4, 0, 4, 3, -8,
-10, -11, 2, 1, 7, -4, 
-0, 2, 1, 12, 5.45, -4, -4, 11,
-12, -6, 1, 1, -11, 11.30, 10, 1,
-4, 5, 9, -5, 10, -4, -5, 8, -8, 
-1, -1, -4, 3, 4, 2, 2, 2, 0,
--4, -4, -4, 0, 13,
-1, 0, 1, 1, 0, 4, 0,
-8, 1, 1, 11, 3, 3,
--1, 0, 0, 5.30, 3, -3,
-1, 2, 1, 1, -2, 8, 5, 3, 7,
--4, 0, 13, 13, -4, 1, 2, 5, 12, 3, 
-3, 4, 0, 0, 10, 5, 11, -4.30,
-3, -4, 4, 4, 0, 0, 3, 0, 
-2, 2, 2);
+function populateCountriesFromJSON(countryElementId){
+	var countryElement = document.getElementById(countryElementId);
+	countryElement.length = 0;
+	countryElement.selectedIndex = 0;
+	for (var i in countriesData) {
+		//console.log(countriesData[i]);
+		countryElement.options[countryElement.length] = new Option(countriesData[i].name);
+	}
+}
